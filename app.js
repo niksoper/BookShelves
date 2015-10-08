2ebb07e8545114587919def69ce1e51d819484ad
niksoper <nsoper@scottlogic.com>
2015-10-08 17:32:11 +0100

    Added stub GET /api/shelves/to-read

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+var router = express.Router();
+router.route('/shelves/to-read')
+    .get(function(req, res) {
+        var books = [{
+            title: 'Title 1',
+            author: 'Alan'
+        }, {
+            title: 'Horse land',
+            author: 'Horse'
+        }]
+        
+        res.json(books);
+    });
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+app.use('/api', router);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
